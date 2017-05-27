0632dd4318ae8fb5c90e615a0b7a9fd43758e2d4
Centro Ático <centroatico@202C11E23168969.local>
2017-05-27 12:53:45 -0500

    cuarta entrada

--- instagramapp/urls.py
+++ instagramapp/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-      url(r'^registrar/', views.registrar ),
-      url (r'$', views.registrar, name = 'registro')
+      url(r'^registrar/$', views.registrar ),
+      url (r'^$', views.registrar, name = 'registro'),
+      url(r'^crear_usuario/$', views.crear_usuario, name = 'crear_usuario'),
+      url(r'^iniciosesion/$', views.iniciosesion, name = 'iniciosesion')
+
       ]
--- instagramapp/views.py
+++ instagramapp/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from instagramapp.models import *
 # Create your views here.
 
 
 def registrar ( request ):
-
     return render( request, 'registro.html')
+
+def crear_usuario ( request ):
+    _email= request.POST['email']
+    _username= request.POST['username']
+    _password= request.POST['password']
+    _name= request.POST['name']
+    print (_email)
+    print(_username)
+    print(_password)
+    print(_name)
+    user=User.objects.create_user( username= _username, email=_email, first_name=_name, password=_password)
+    myUser = MiUsuario( usuario=user )
+    myUser.save()
+    print (user)
+    print (user.password)
+    return redirect ('iniciosesion')
+def iniciosesion ( request ):
+    return render(request, 'iniciosesion.html')
+
+def maindpage ( request ):
+    return render (request, 'paginaprincipal.html')
